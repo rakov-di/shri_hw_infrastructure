539aa6bdea3129bb65373b588f3ffac5425e6475
Dmitry Rakov <rakov.di@gmail.com>
2020-04-20 01:27:11 +0300

    [+] Добавляет билд-агенту обработку неотвечающего билд-сервера

--- build-agent/controllers/bsController.js
+++ build-agent/controllers/bsController.js
@@ -1,12 +1,25 @@
 const { registerAgent, sendBuildResults } = require('../api');
 
+// Если билд-сервер не ответил - пробуем еще ${maxApiBSErrorsCount} раз с коротким интервалом,
+// если все еще не отвечает - продолжаем спрашивать пока не ответит, но уже через длинный интервал
+
+let apiBSErrorsCount = 0; // текущее число ошибок при обращении к билд-серверу
+const maxApiBSErrorsCount = 2; // максимальное число обращений с ошибками, после которого надо увеличивать интервал обращения
+const shortInterval = 2000; // короткий интервал повторения запроса
+const longInterval = 10000; // увеличиенный интервал посторения запроса
+
 const bsControllers = {
   async registerAgent() {
     try {
       const response = await registerAgent();
       console.log(response.data.message);
+      apiBSErrorsCount = 0;
     } catch(err) {
       console.error('Can not register build agent because of error: ', err.message);
+      apiBSErrorsCount++;
+      const interval = (apiBSErrorsCount <= maxApiBSErrorsCount) ? shortInterval: longInterval;
+      console.log(`Next try in an ${interval} ms`);
+      setTimeout(bsControllers.registerAgent, interval);
     }
   },
 
@@ -14,8 +27,13 @@ const bsControllers = {
     try {
       const response = await sendBuildResults(params);
       console.log(response.data.message);
+      apiBSErrorsCount = 0;
     } catch(err) {
-      console.error('Can not send result ti build server because of error: ', err.message);
+      apiBSErrorsCount++;
+      console.error(`Can't send result ti build server because of error: ${err.message}`);
+      const interval = (apiBSErrorsCount <= maxApiBSErrorsCount) ? shortInterval: longInterval;
+      console.log(`Next try in an ${interval} ms`);
+      setTimeout(bsControllers.sendBuildResults, interval);
     }
   }
 };
